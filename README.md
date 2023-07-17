10dd3d302d534a495fe031d2bb4aa118bad4cac9
Floride125-art <flo.tuyisenge@gmail.com>
2023-07-17 10:48:37 +0530

    completed the paper rock scissor project

---
+++ README.md
@@ -0,0 +1,7 @@
+## paper rock scissor project
+This is a simple website that consists the simple game of rock, paper, and scissor
+## Technologies used
+JavaScript, Html and CSS
+## installation and running 
+clone the project by git clone  in your local machine and run it or copy deployed live link of project and then run it in your browser.
+
---
+++ app.js
@@ -0,0 +1,68 @@
+var btn = document.querySelectorAll(".btn");
+var user_score_count = 0;
+var cpu_score_count = 0;
+Array.from(btn).forEach((e) => {
+  e.addEventListener("click", (e) => {
+    document.querySelector(".player-1").innerHTML = e.target.getAttribute(
+      "data-value"
+    );
+    var num = Math.floor(Math.random() * 3);
+    if (num == 0) {
+      document.querySelector(".cpu").innerHTML = "🪨";
+    }
+    if (num == 1) {
+      document.querySelector(".cpu").innerHTML = "📄";
+    }
+    if (num == 2) {
+      document.querySelector(".cpu").innerHTML = "✂️";
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "🪨" &&
+      document.querySelector(".cpu").innerHTML == "✂️"
+    ) {
+      cpu_score_count = cpu_score_count + 1;
+      document.querySelector(".cpu-score").innerHTML = cpu_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "✂️" &&
+      document.querySelector(".cpu").innerHTML == "🪨"
+    ) {
+      user_score_count = user_score_count + 1;
+      document.querySelector(".player-score").innerHTML = user_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "🪨" &&
+      document.querySelector(".cpu").innerHTML == "📄"
+    ) {
+      cpu_score_count = cpu_score_count + 1;
+      document.querySelector(".cpu-score").innerHTML = cpu_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "📄" &&
+      document.querySelector(".cpu").innerHTML == "🪨"
+    ) {
+      user_score_count = user_score_count + 1;
+      document.querySelector(".player-score").innerHTML = user_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "📄" &&
+      document.querySelector(".cpu").innerHTML == "✂️"
+    ) {
+      cpu_score_count = cpu_score_count + 1;
+      document.querySelector(".cpu-score").innerHTML = cpu_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML == "✂️" &&
+      document.querySelector(".cpu").innerHTML == "📄"
+    ) {
+      user_score_count = user_score_count + 1;
+      document.querySelector(".player-score").innerHTML = user_score_count;
+    }
+    if (
+      document.querySelector(".player-1").innerHTML ==
+      document.querySelector(".cpu").innerHTML
+    ) {
+      alert("draw");
+    }
+  });
+});
